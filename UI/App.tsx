da5b987147389c00a45aacf7ff76693a8a4a9a77
ilhambagoes <ilham.tripoetra@mncbank.co.id>
2023-05-23 11:14:38 +0700

    Add new file and change directory for App.tsx

---
+++ UI/App.tsx
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+  StyleSheet
+} from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <Text style={styles.title}>Seol In Ah</Text>
+      <View>
+        <Text style={styles.description}>
+          Seol In-Ah, juga dikenal sebagai Seorina,
+          adalah aktris asal Korea Selatan yang paling dikenal karena perannya
+          sebagai peran pendukung dalam dua serial televisi populer Mr. Queen dan Business Proposal
+        </Text>
+        <Image
+          source={{ uri: 'https://assets.pikiran-rakyat.com/crop/0x202:1080x881/x/photo/2022/03/11/4171911473.jpg' }}
+          style={styles.image}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginHorizontal: 12,
+          paddingStart: 12
+        }}
+        placeholder="Pendapat tentang Seol In Ah ?"
+      />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 12,
+    fontSize: 25,
+    textAlign: 'center',
+  },
+  description: {
+    marginHorizontal: 12,
+    textAlign: 'justify'
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginVertical: 12,
+    marginHorizontal: 12,
+    alignSelf: 'center'
+  }
+})
+
+export default App;
