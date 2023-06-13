bef0ef1eae22f6db91f0042b3012af74c0d62931
ilhambagoes <ilham.tripoetra@mncbank.co.id>
2023-06-13 17:43:13 +0700

    Implement card layout in news page

--- UI/NewsPage.tsx
+++ UI/NewsPage.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ActivityIndicator, FlatList, Text, View, Image, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
 
 const URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f501615931a3413a9a5c6a0985679f88"
 
@@ -59,19 +60,23 @@ export default class App extends Component {
             //keyExtractor={({id}) => id}
             renderItem={({ item }) => (
               <View>
-                {item.urlToImage ? (
-                  <Image
-                    source={{ uri: `${item.urlToImage}` }}
-                    style={styles.image}
-                  />
-                ) : (
-                  <Image
-                    source={{ uri: 'https://widodaren.ngawikab.go.id/wp-content/uploads/2020/11/placeholder.png' }}
-                    style={styles.image}
-                />
-                )}
-                <Text style={styles.title}>{item.title}</Text>
-                <Text style={styles.description}>{item.description}</Text>
+                <Card style={styles.card}>
+                  <Card.Content>
+                    {item.urlToImage ? (
+                      <Image
+                        source={{ uri: `${item.urlToImage}` }}
+                        style={styles.image}
+                      />
+                    ) : (
+                      <Image
+                        source={{ uri: 'https://widodaren.ngawikab.go.id/wp-content/uploads/2020/11/placeholder.png' }}
+                        style={styles.image}
+                      />
+                    )}
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.description}>{item.description}</Text>
+                  </Card.Content>
+                </Card>
               </View>
             )}
           />
@@ -82,17 +87,24 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    marginVertical: 12,
+    marginHorizontal: 12
+  },
   title: {
     fontSize: 15,
     color: 'black',
+    marginHorizontal: 12
   },
   description: {
-    marginBottom: 12
+    marginBottom: 12,
+    marginHorizontal: 12
   },
   image: {
-    width: 500,
+    width: 400,
     height: 200,
-    marginVertical: 12,
+    marginBottom: 12,
     marginHorizontal: 12,
     alignSelf: 'center'
   }
